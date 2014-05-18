2248a910cec4bcacb4d73ce966f1b30503db722a
jdoi <jdoi.work@gmail.com>
2014-05-18 16:02:15 +0900

    angularのajaxを試したが諦めた

--- Tenchi/fay/Home.hs
+++ Tenchi/fay/Home.hs
@@ -35,15 +35,34 @@ doMyAddr = ffi "console.log(%1 + ':' + %2)"
 doMyAddr1 :: MyAddr -> Fay ()
 doMyAddr1 = ffi "console.log(%1)"
 
+consoleLog :: a -> Fay ()
+consoleLog = ffi "console.log(%1)"
+
 data NgScope
 
 createHomeCtrl :: (NgScope -> Fay ()) -> Fay ()
 createHomeCtrl = ffi "app.controller('HomeCtrl', function($scope){ %1($scope);})"
 
-setJdoi :: NgScope -> Fay ()
-setJdoi scope = do
-    (ffi "%1.home = {}" :: NgScope -> Fay ()) scope
-    (ffi "%1.home.name = 'jdoi'"  :: NgScope -> Fay ()) scope
+data HomeName = HomeName 
+              { name :: String
+              }
+jdoiHomeName = HomeName "jdoi"
+
+setJdoi :: NgScope -> HomeName -> Fay ()
+setJdoi = ffi "%1.home = %2" 
+
+setHogeNg :: NgScope -> String -> Fay ()
+setHogeNg = ffi "%1.hoge = %2 " 
+
+setJdoiWithHoge :: NgScope -> Fay()
+setJdoiWithHoge scope = do
+    setJdoi scope jdoiHomeName
+    call GetHoge $ \t -> do 
+      setHogeNg scope $ toString t
+      applyScope scope
+
+applyScope :: NgScope -> Fay ()
+applyScope = ffi "%1.$apply()"
 
 main :: Fay ()
 main = do
@@ -56,5 +75,5 @@ main = do
     call GetHoge $ setHoge . toString
     call GetMyAddr $ \(MyAddr name mail) -> doMyAddr name mail
     call GetMyAddr $ doMyAddr1
-    createHomeCtrl setJdoi
+    createHomeCtrl $ setJdoiWithHoge -- flip setJdoi jdoiHomeName
 
